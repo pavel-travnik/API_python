cf60eea60a68df5cd3442a3282889c17c4afaeb2
pavel-travnik <pavel.travnik@uniqa.cz>
2026-03-30 12:11:38 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/get_dps_data", methods=["GET"])
+def get_dps_data():
+    data = [
+        {
+            "isin": "UNIQADPS001",
+            "fond": "Akciový ú.f. UNIQA p.s. a.s.",
+            "date": "2026-03-13",
+            "value": 1.9152,
+            "currency": "CZK"
+        },
+        {
+            "isin": "UNIQADPS001",
+            "fond": "Akciový ú.f. UNIQA p.s. a.s.",
+            "date": "2026-03-20",
+            "value": 1.8678,
+            "currency": "CZK"
+        }
+    ]
+    return jsonify(data)
+
+# Azure Web App spustí Gunicorn s app:app
